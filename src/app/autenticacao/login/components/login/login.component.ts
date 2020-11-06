4e6c063e927dd334037c86d2864372f6a9351e78
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-11-05 21:05:54 -0300

    Tela de Login criada

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // componentes relacionados ao formulário
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup; // FormGroup faz agrupamento de componentes no caso de login tem login e de senha, e faz mapeamento com formulario html
 
+  constructor(
+    private fb: FormBuilder, // ajuda a criar e mapear o formulário
+    private snackBar: MatSnackBar, // mensagem de erro ou sucesso
+    //private router: Router
+  ) { }
+
+  // método para executar logo na sequencia que construtor cria o objeto
   ngOnInit(): void {
+    this.gerarForm();
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({ // dentro do group passará objeto Json com toda configuração do formulario
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  // Ação logar
+  logar() {
+    if (this.form.invalid) {
+      this.snackBar.open(
+        "Dados Inválidos", "Erro", { duration: 5000 });
+      return;
+    }
+    alert(JSON.stringify(this.form.value));
   }
 
 }
--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -1,6 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // responsável por disponibilizar o router-outlet directiva 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { LoginComponent, LogarComponent } from './components';
 
@@ -11,7 +22,16 @@ import { LoginComponent, LogarComponent } from './components';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule, // Módulo responsavel por criar campos de entrada de dados
+    MatButtonModule, // Módulo responsavel por criar os botões
+    MatListModule, // ajudar a criar a interface no formato de lista ou de colunas
+    MatTooltipModule, // Módulo que usamos para passar o mouse e exibir informação 
+    MatIconModule, // Módulo que usamos para criar um icone formato de interrogação exibira mensagem de ajuda
+    MatSnackBarModule, // Campo de alerta, sucesso, erro
+    FlexLayoutModule // Tornar componentes responsivos
   ]
 })
 export class LoginModule { }
