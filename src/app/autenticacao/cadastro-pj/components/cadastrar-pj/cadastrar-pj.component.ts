006b9fe5c8c3a2116d504cf0e3f255949784c077
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-11-06 11:58:44 -0300

    Criando o model de cadastro de pessoa jurídica

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar'; // Para mensagens de erro
 import { Router } from '@angular/router';
 
+import { CadastroPj } from '../../models';
+
 @Component({
   selector: 'app-cadastrar-pj',
   templateUrl: './cadastrar-pj.component.html',
@@ -36,7 +38,9 @@ export class CadastrarPjComponent implements OnInit {
   cadastrarPj() {
   	if (this.form.invalid) {
   		return;
-  	}
-    alert(JSON.stringify(this.form.value));
+    }
+    
+    const cadastroPj: CadastroPj = this.form.value;
+    alert(JSON.stringify(cadastroPj));
   }
 }
--- src/app/autenticacao/cadastro-pj/index.ts
+++ src/app/autenticacao/cadastro-pj/index.ts
@@ -1,3 +1,4 @@
 // Fazer exports para níveis superiores (facilitar no import )
 export * from './cadastro-pj.module';
 export * from './cadastro-pj-routing.module';
+export * from './models';
