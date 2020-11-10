b77f1a523ed988e75854aed201b9a7cda6ae720b
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-11-10 01:05:52 -0300

    Model de cadastro de pessoa física implementado

--- src/app/autenticacao/cadastro-pf/index.ts
+++ src/app/autenticacao/cadastro-pf/index.ts
@@ -1,5 +1,4 @@
-import { from } from 'rxjs';
-
 // Fazer exports para níveis superiores (facilitar no import )
 export * from './cadastro-pf.module';
 export * from './cadastro-pf-routing.module';
+export * from './models';
