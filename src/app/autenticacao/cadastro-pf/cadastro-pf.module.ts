e92ad01b6f58a41c059af36d165f672d20e8ed4f
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-11-10 00:54:57 -0300

    Criando o formulário de cadastro de pessoa física

--- src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
+++ src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // usado em router-outlet nos componentes
 import { CadastrarPfComponent, CadastroPfComponent } from './components';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms'; // responsável pela criação do formulário
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +23,16 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     CommonModule,
     FlexLayoutModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    SharedModule
   ]
 })
 export class CadastroPfModule { }
