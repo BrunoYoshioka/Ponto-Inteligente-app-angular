50d2c4039409cad6fe61caace7748125b2741cd8
Bruno Yuji Yoshioka <bruno.yuji1995@gmail.com>
2020-11-10 14:56:20 -0300

    Implementando o componente de lançamento

--- src/app/funcionario/components/lancamento/lancamento.component.ts
+++ src/app/funcionario/components/lancamento/lancamento.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'; // para mensagens de erro ou sucesso
+import { Router } from '@angular/router'; // para redirecionamento das telas
+
+import {
+  Tipo,
+} from '../../../shared';
+
+import * as moment from 'moment'; // importação da biblioteca externa
+
+declare var navigator: any; // utilizar esse objeto externo para obter geolocalização para pegar a posição do GPS do usuário
 
 @Component({
   selector: 'app-lancamento',
@@ -7,9 +17,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoComponent implements OnInit {
 
-  constructor() { }
+  private dataAtualEn: string;
+  dataAtual: string;
+  geoLocation: string;
+  ultimoTipoLancado: string;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.dataAtual = moment().format('DD/MM/YYYY HH:mm:ss');
+  	this.dataAtualEn = moment().format('YYYY-MM-DD HH:mm:ss');
+  	this.obterGeoLocation();
+    this.ultimoTipoLancado = '';
+    this.obterUltimoLancamento();
   }
 
+  obterGeoLocation(): string {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position =>
+        	this.geoLocation = `${position.coords.latitude},${position.coords.longitude}`);
+    }
+    return ''; // caso não consiga obter a localização retorna nulo
+  }
+
+  iniciarTrabalho() {
+    this.cadastrar(Tipo.INICIO_TRABALHO);
+  }
+
+  terminarTrabalho() {
+    this.cadastrar(Tipo.TERMINO_TRABALHO);
+  }
+
+  iniciarAlmoco() {
+    this.cadastrar(Tipo.INICIO_ALMOCO);
+  }
+
+  terminarAlmoco() {
+    this.cadastrar(Tipo.TERMINO_ALMOCO);
+  }
+
+  obterUltimoLancamento() {
+    this.ultimoTipoLancado = '';
+  }
+
+  cadastrar(tipo: Tipo) {
+    alert(`Tipo: ${tipo}, dataAtualEn: ${this.dataAtualEn},
+      geolocation: ${this.geoLocation}`);
+  }
+
+  obterUrlMapa(): string {
+  	return "https://www.google.com/maps/search/?api=1&query=" +
+  		this.geoLocation;
+  }
+
+  // Métodos para saber quando existir botão ou não
+  exibirInicioTrabalho(): boolean {
+  	return this.ultimoTipoLancado == '' ||
+  		this.ultimoTipoLancado == Tipo.TERMINO_TRABALHO;
+  }
+
+  exibirTerminoTrabalho(): boolean {
+  	return this.ultimoTipoLancado == Tipo.INICIO_TRABALHO ||
+  		this.ultimoTipoLancado == Tipo.TERMINO_ALMOCO;
+  }
+
+  exibirInicioAlmoco(): boolean {
+  	return this.ultimoTipoLancado == Tipo.INICIO_TRABALHO;
+  }
+
+  exibirTerminoAlmoco(): boolean {
+  	return this.ultimoTipoLancado == Tipo.INICIO_ALMOCO;
+  }
 }
--- src/app/funcionario/funcionario.module.ts
+++ src/app/funcionario/funcionario.module.ts
@@ -2,6 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // router-outlet
 import { FlexLayoutModule } from '@angular/flex-layout'; // possui centralização de componentes
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
 
 import { ListagemComponent, LancamentoComponent, FuncionarioComponent } from './components';
 
@@ -14,7 +26,18 @@ import { ListagemComponent, LancamentoComponent, FuncionarioComponent } from './
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule
   ]
 })
 export class FuncionarioModule { }
